59754bef9b5dbc982fa46653d2e3ec8fc51235a6
cloud-dph <clouds.dph@gmail.com>
2025-03-07 08:54:46 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,33 @@
+const episodes = {
+    "season17": [
+        {
+            "title": "Ultra Mixer / Sneeze Busters!",
+            "ep_no": "S17 E1",
+            "date": "1 Mar 2020",
+            "duration": "24m",
+            "description": "Doraemon's latest gadget combines two objects into one.",
+            "image": "https://example.com/doraemon1.jpg",
+            "watch_url": "https://example.com/watch-episode1"
+        },
+        {
+            "title": "Demons In! Luck In! Bean / Good-Bye Shizuka",
+            "ep_no": "S17 E2",
+            "date": "1 Mar 2020",
+            "duration": "23m",
+            "description": "On the day of the bean-throwing ceremony, Suneo asks Nobita for help.",
+            "image": "https://example.com/doraemon2.jpg",
+            "watch_url": "https://example.com/watch-episode2"
+        }
+    ],
+    "season18": [
+        {
+            "title": "New Episode Title",
+            "ep_no": "S18 E1",
+            "date": "5 Mar 2021",
+            "duration": "22m",
+            "description": "An exciting new adventure for Doraemon and friends!",
+            "image": "https://example.com/doraemon3.jpg",
+            "watch_url": "https://example.com/watch-episode3"
+        }
+    ]
+};
