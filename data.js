7b3e2f9844876bdf6233fb91b4f189c3b518cd35
cloud-dph <clouds.dph@gmail.com>
2025-03-18 11:41:05 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,4 +1,18 @@
 const users = [
-    { name: "Vinayak", fullName: "Vinayak Sharma", mobile: "9876543210", password: "123456", address: "Delhi, India" },
-    { name: "Rahul", fullName: "Rahul Verma", mobile: "8765432109", password: "password123", address: "Mumbai, India" }
+    { 
+        name: "Vinayak", 
+        fullName: "Vinayak Sharma", 
+        mobile: "9876543210", 
+        password: "123456", 
+        dob: "1995-05-15", 
+        address: "Delhi, India" 
+    },
+    { 
+        name: "Rahul", 
+        fullName: "Rahul Verma", 
+        mobile: "8765432109", 
+        password: "password123", 
+        dob: "1998-09-25", 
+        address: "Mumbai, India" 
+    }
 ];
