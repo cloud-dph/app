b6d611167a01642152ec3e5e7b5fa01fc5e8244a
cloud-dph <clouds.dph@gmail.com>
2025-03-25 13:46:31 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
 const users = {
-    "9784669701": { name: "Bhawna", password: "000", color: "#ff5733", pan: "224621" },
-    "8005811475": { name: "Vinayak", password: "000", color: "#33ff57", pan: "994455" }
+    "9784669701": { name: "Bhawna", password: "000", color: "#ff5733", pan: "224621", email: "nigambhawna02@gmail.com" },
+    "8005811475": { name: "Vinayak", password: "000", color: "#33ff57", pan: "994455",  }
 };
