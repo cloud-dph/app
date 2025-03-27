fa7b2e822630b97148d8079c3ef8f6240d11946b
cloud-dph <clouds.dph@gmail.com>
2025-03-27 13:59:24 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 const users = {
-    "9784669701": { name: "Bhawna", password: "000", color: "#ff5733", pan: "224621", email: "nigambhawna02@gmail.com", subscription: "yes" },
-    "8005811475": { name: "Vinayak", password: "000", color: "#33ff57", pan: "994455", subscription: "no"  },
-    "8949972344": { name: "Vaibhav", password: "000", color: "#000", pan: "994455", subscription: "no"  },
-    "9413806768": { name: "Ashaish", password: "000", color: "#33ff57", pan: "994455", subscription: "no"  }
+    "9784669701": { name: "Bhawna", password: "000", color: "#ff5733", pan: "224621", email: "nigambhawna02@gmail.com", subscription: "Mobile" },
+    "8005811475": { name: "Vinayak", password: "000", color: "#33ff57", pan: "994455", subscription: "no Active Plan"  },
+    "8949972344": { name: "Vaibhav", password: "000", color: "#000", pan: "994455", subscription: "no Active Plan"  },
+    "9413806768": { name: "Ashaish", password: "000", color: "#33ff57", pan: "994455", subscription: "no Active Plan"  }
 };
