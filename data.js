bafc5c03af78a5ec8c13c0092ca25170f99bfce5
cloud-dph <clouds.dph@gmail.com>
2025-03-30 11:39:20 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,6 +1,13 @@
 const users = {
-    "9784669701": { name: "Bhawna", password: "000", color: "#ff5733", pan: "224621", email: "nigambhawna02@gmail.com", subscription: "Mobile" },
-    "8005811475": { name: "Vinayak", password: "000", color: "#33ff57", pan: "994455", subscription: "no Active Plan"  },
-    "8949972344": { name: "Vaibhav", password: "000", color: "#000", pan: "994455", subscription: "no Active Plan"  },
-    "9413806768": { name: "Ashaish", password: "000", color: "#33ff57", pan: "994455", subscription: "no Active Plan"  }
+    "9784669701": { name: "Bhawna Nigam", password: "000", color: "#ff5733", pan: "224621", email: "nigambhawna02@gmail.com", subscription: "Mobile" },
+    "8005811475": { name: "Vinayak Nigam", password: "000", color: "#33ff57", pan: "994455", subscription: "no Active Plan"  },
+    "8949972344": { name: "Vaibhav Nigam", password: "000", color: "#000", pan: "994455", subscription: "no Active Plan"  },
+    "9413806768": { name: "Ashaish Nigam", password: "000", color: "#33ff57", pan: "994455", subscription: "no Active Plan"  },
+    "9785100969": { name: "Meenu Nigam", password: "000", color: "#33ff57", pan: "994455", subscription: "no Active Plan"  },
+    "9588989211": { name: "Anubhav Nigam", password: "000", color: "#33ff57", pan: "994455", subscription: "no Active Plan"  },
+    "9414489615": { name: "Ram Prsad Nigam", password: "000", color: "#33ff57", pan: "994455", subscription: "no Active Plan"  },
+    "7737076626": { name: "Prince Lodha", password: "000", color: "#33ff57", pan: "994455", subscription: "no Active Plan"  },
+    "9543286136": { name: "Dheeraj", password: "000", color: "#33ff57", pan: "994455", subscription: "no Active Plan"  },
+    "9602682134": { name: "Tanmaya Sharma", password: "000", color: "#33ff57", pan: "994455", subscription: "no Active Plan"  },
+    "7726959232": { name: "Anita Sharma", password: "000", color: "#33ff57", pan: "994455", subscription: "no Active Plan"  },
 };
